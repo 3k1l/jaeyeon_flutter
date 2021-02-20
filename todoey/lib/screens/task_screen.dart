1dcceb20e0e9d0574bb9a9c420c8c9f80a16c364
jaeyeon <jaeyeon423@gmail.com>
2021-02-20 13:10:00 +0900

    new Todoey

--- todoey/lib/screens/task_screen.dart
+++ todoey/lib/screens/task_screen.dart
@@ -6,16 +6,14 @@ class TaskScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       body: Container(
-        padding: EdgeInsets.only(top: 60.0, left: 30.0, bottom: 30.0),
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
         child: Column(
           children: [
             CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 30.0,
-              ),
-              backgroundColor: Colors.white,
+              child: Icon(Icons.list, size: 35.0,),
               radius: 30.0,
+              backgroundColor: Colors.white,
             ),
             Text('Todoey'),
           ],
