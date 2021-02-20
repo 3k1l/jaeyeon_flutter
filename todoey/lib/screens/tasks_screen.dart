9da6bc230bbec48870b4d1f758cd94671c3a93c7
Jae Yeon <jaeyeon423@gmail.com>
2021-02-20 10:52:48 +0900

    circleAvatar

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0, left: 30.0, bottom: 30.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            Text('Todoey'),
+          ],
+        ),
+      ),
+    );
   }
 }
