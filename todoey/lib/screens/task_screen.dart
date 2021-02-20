4697c6d02d830d841a59f639e5e5cedcaaabbd23
jaeyeon <jaeyeon423@gmail.com>
2021-02-20 13:22:47 +0900

    set UI

--- todoey/lib/screens/task_screen.dart
+++ todoey/lib/screens/task_screen.dart
@@ -5,19 +5,52 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          children: [
-            CircleAvatar(
-              child: Icon(Icons.list, size: 35.0,),
-              radius: 30.0,
-              backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding:
+                EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 35.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  radius: 30.0,
+                  backgroundColor: Colors.white,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
             ),
-            Text('Todoey'),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              // color: Colors.white, //crash
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0))
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
