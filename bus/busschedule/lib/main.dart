aa07a37b58cae22a90a9b6223bd527562fdf22d8
Jae Yeon <jaeyeon423@gmail.com>
2021-03-06 23:31:51 +0900

    bus ui

--- bus/busschedule/lib/main.dart
+++ bus/busschedule/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'route.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,9 +7,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('bus schedule'),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('bus schedule'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 200,),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'location'
+                ),
+              ),
+            ),
+            TextButton(onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            }, child: Text("click"),)
+          ],
         ),
       ),
     );
